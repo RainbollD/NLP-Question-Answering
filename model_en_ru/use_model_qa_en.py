4b19c1ae5dd9dbef6f3e726f185e02394ce27304
Lev <rogulev0805@gmail.com>
2025-05-14 11:58:02 +0300

    comment

--- model_en_ru/use_model_qa_en.py
+++ model_en_ru/use_model_qa_en.py
@@ -1,20 +1,23 @@
 import torch
 from transformers import AutoTokenizer, BertForQuestionAnswering, logging
 
-from translator import *
+from model_en_ru.translator import *
+from get_configs import Paths
 
 logging.set_verbosity_error()
 
 
 def get_model_tokenizer():
-    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-large-uncased-whole-word-masking-finetuned-squad",
-                                              ignore_mismatched_sizes=True)
-    model = BertForQuestionAnswering.from_pretrained(
-        "google-bert/bert-large-uncased-whole-word-masking-finetuned-squad", ignore_mismatched_sizes=True)
+    """Get tokenizer and model using name model from configs: model_name_en_ru_qa"""
+    path_model = Paths().get_model_en_ru()
+
+    tokenizer = AutoTokenizer.from_pretrained(path_model, ignore_mismatched_sizes=True)
+    model = BertForQuestionAnswering.from_pretrained(path_model, ignore_mismatched_sizes=True)
     return tokenizer, model
 
 
 def predict_en_qa(question, text):
+    """Get prediction model in ENGLISH"""
     tokenizer, model = get_model_tokenizer()
 
     inputs = tokenizer(question, text, return_tensors="pt")
@@ -34,10 +37,13 @@ def predict_en_qa(question, text):
 
 
 def predict_from_ru_en(question_ru, text_ru):
+    # Translate data to english
     question_en, text_en = translate_ru_en(question_ru), translate_ru_en(text_ru)
 
+    # Get prediction in english
     answer_en = predict_en_qa(question_en, text_en)
 
+    # Translate prediction in russia
     answer_ru = translate_en_ru(answer_en)
 
     return answer_ru
