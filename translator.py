b8049d2bf6aad2476ae9005bb6303cc205987cb6
Lev <rogulev0805@gmail.com>
2025-05-23 18:12:03 +0300

    add function to choose languages and translator

--- translator.py
+++ translator.py
@@ -9,6 +9,11 @@ def translate_ru_en(text):
     return ts.translate_text(query_text=text, from_language='ru', to_language='en', translator='yandex')
 
 
+def translate_for_all(text, from_language, to_language, translator='yandex'):
+    return ts.translate_text(query_text=text, from_language=from_language, to_language=to_language,
+                             translator=translator)
+
+
 if __name__ == '__main__':
     en_text = 'match percentages'
     print(translate_en_ru(en_text))
